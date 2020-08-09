fc6f159d7d2fdd3714863cc318feb00c1875a97e
zzrjezz <58676425+zzrjezz@users.noreply.github.com>
2020-08-09 22:16:28 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+
+<template>
+  <div id="app">
+  <my-header></my-header>
+  <my-header></my-header>
+  <my-header></my-header>
+    <div id="nav">
+      <router-link to="/">Home</router-link> 
+      <router-link to="/about">推荐音乐</router-link>
+      <router-link to="/my">热歌榜</router-link>
+      <router-link to="/my-element">搜索</router-link>
+
+    </div>
+    <router-view/>
+     name:{{name}}   show:{{show}} 
+  </div>
+</template>
+
+<script>
+import myHeader from '@/views/my-header'
+export default {
+  name:"header",
+  components: {
+myHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  margin-left: 1em;
+}
+
+#nav a.router-link-exact-active {
+  color: #D43C33;
+}
+</style>
---
+++ src/views/my-header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="my-header">
+    <div class="div2">
+    
+    <h1>网易云音乐</h1>
+    </div>
+    <div class="div3">
+      <Button class="header-button">下载app</Button>
+      </div>
+    
+  </div>
+</template>
+<script>
+
+export default {
+  name:'my-header'
+}
+</script>
+<style scoped>
+div.my-header{
+  background-color:#D43C33;
+  height: 10vh;
+}
+div.my-header div.div2{
+  /* border: 1px solid #000; */
+  width: 30vw;
+  float: left;
+}
+div.my-header div.div3{
+  margin-top: 2.5vh;
+  /* background-color: violet; */
+  /* border: 1px solid #000; */
+  width: 20vw;
+  float: right;
+}
+div.my-header .div3 button{
+  color: #D43C33;
+  border-radius: 15px;
+  width: 100%;
+  /* height: 5vh; */
+  margin-top: 1vh;
+}
+div.my-header div.div2 h1{
+  color: aliceblue;
+  font-size: 20px;
+}
+
+</style>
